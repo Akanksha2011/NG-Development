5d0852fa0f407207d0651423f5968df39ab33f40
Akanksha Pandey <akankshapandeyb2001@gmail.com>
2024-08-13 12:45:21 +0530

    Add

---
+++ switchCase.js
@@ -0,0 +1,21 @@
+let day = 9;
+let result;
+switch(day)
+{
+    case 1: result = "Monday";
+    break;
+    case 2: result = 'Tuesday';
+    break;
+    case 3: result = "Wednesday";
+    break;
+    case 4: result = 'Thursday';
+    break;
+    case 5: result = "Friday";
+    break;
+    case 6: result = 'Saturday';
+    break;
+    case 7: result = "Sunday";
+    break;
+    default: result = "Not a valid day";
+}
+console.log(result);
